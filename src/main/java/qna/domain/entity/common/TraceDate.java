3ef99f85d876235bf8e7c659d96391944600ef0a
hun <ini8262@itnomads.com>
2021-06-07 14:09:43 +0900

    feat : 엔티티의 생성&수정 시간관리

--- src/main/java/qna/domain/entity/common/TraceDate.java
+++ src/main/java/qna/domain/entity/common/TraceDate.java
@@ -1,17 +1,25 @@
 package qna.domain.entity.common;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class TraceDate {
 
+    @CreatedDate
     @Column(nullable = false)
     protected LocalDateTime createdAt;
 
+    @LastModifiedDate
     protected LocalDateTime updatedAt;
 
     public TraceDate() {
